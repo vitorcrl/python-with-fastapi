0c99053131493285aeef077ac6b69b51dccb0c25
Vitor Carlos <vitorcarlos1312@gmail.com>
2024-08-29 15:22:53 -0300

    feat: update crud and route items

---
+++ app/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_item(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
+
+def get_items(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+def create_item(db: Session, item: schemas.ItemCreate):
+    db_item = models.Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def delete_item(db: Session, item_id: int):
+    db_item = get_item(db, item_id)
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+    return db_item
+
+def update_item(db: Session, item_id: int, updated_item: schemas.ItemCreate):
+    db_item = get_item(db, item_id)
+    if db_item:
+        for key, value in updated_item.dict().items():
+            setattr(db_item, key, value)
+        db.commit()
+        db.refresh(db_item)
+    return db_item
---
+++ app/routers/items.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List
+from sqlalchemy.orm import Session
+from .. import crud, models, schemas
+from ..database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+# Dependência para obter a sessão do banco de dados
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+@router.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+@router.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@router.put("/items/{item_id}", response_model=schemas.Item)
+def update_item(item_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = crud.update_item(db, item_id=item_id, updated_item=item)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
+@router.delete("/items/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.delete_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"detail": "Item deleted"}
