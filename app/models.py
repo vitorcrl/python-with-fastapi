fc8a3c60326275ac90d53da7deb1688827d700f5
Vitor Carlos <vitorcarlos1312@gmail.com>
2024-09-04 02:22:45 -0300

    feat: update modal and testdb

--- app/models.py
+++ app/models.py
@@ -9,3 +9,14 @@ class Item(Base):
     description = Column(String, index=True)
     price = Column(Float)
     tax = Column(Float, default=0)
+
+
+class Pipocas(Base):
+    __tablename__ = "popcorns"
+
+    id = Column(Integer, primary_key=True, index=True)
+    create_on = Column(String, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    weight = Column(Float)
+    quantity = Column(Float)
